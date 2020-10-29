b7ba387c71488cd5056f0880e8dd2a9cf1338910
Jared Mollenkamp <jamollenkamp@gmail.com>
2020-10-29 18:56:18 -0500

    Convert from double to decimal Add Exception to InterestEarned

--- abc-bank/Account.cs
+++ abc-bank/Account.cs
@@ -22,16 +22,17 @@ namespace abc_bank
             this.transactions = new List<Transaction>();
         }
 
-        public void Deposit(double amount) 
+        public void Deposit(decimal amount) 
         {
             if (amount <= 0) {
                 throw new ArgumentException("amount must be greater than zero");
             } else {
                 transactions.Add(new Transaction(amount));
             }
+
         }
 
-        public void Withdraw(double amount) 
+        public void Withdraw(decimal amount) 
         {
             if (amount <= 0) {
                 throw new ArgumentException("amount must be greater than zero");
@@ -40,36 +41,33 @@ namespace abc_bank
             }
         }
 
-        public double InterestEarned() 
+        public decimal InterestEarned() 
         {
-            double amount = sumTransactions();
+            decimal amount = sumTransactions();
             switch(accountType){
-                case SAVINGS:
+                case AccountType.CHECKING:
+                    return amount * 0.001M;
+                case AccountType.SAVINGS:
                     if (amount <= 1000)
-                        return amount * 0.001;
+                        return amount * 0.001M;
                     else
-                        return 1 + (amount-1000) * 0.002;
-    //            case SUPER_SAVINGS:
-    //                if (amount <= 4000)
-    //                    return 20;
-                case MAXI_SAVINGS:
+                        return 1 + (amount-1000) * 0.002M;
+                case AccountType.MAXI_SAVINGS:
                     if (amount <= 1000)
-                        return amount * 0.02;
+                        return amount * 0.02M;
                     if (amount <= 2000)
-                        return 20 + (amount-1000) * 0.05;
-                    return 70 + (amount-2000) * 0.1;
+                        return 20 + (amount-1000) * 0.05M;
+                    return 70 + (amount-2000) * 0.1M;
                 default:
-                    return amount * 0.001;
-            }
-        }
+                    throw new Exception("Unknown Account Type");
 
-        public double sumTransactions() {
+        public decimal sumTransactions() {
            return CheckIfTransactionsExist(true);
         }
 
-        private double CheckIfTransactionsExist(bool checkAll) 
+        private decimal CheckIfTransactionsExist(bool checkAll) 
         {
-            double amount = 0.0;
+            decimal amount = 0;
             foreach (Transaction t in transactions)
                 amount += t.amount;
             return amount;
