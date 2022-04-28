71bf558bfbab4dbbf16b46c4ffeb42f6d54c1c4d
Andrew Dulac <dulacand@ksu.edu>
2022-04-27 22:04:56 -0500

    All tests pass... Last one is ignored for some reason.

--- abc-bank/Customer.cs
+++ abc-bank/Customer.cs
@@ -84,7 +84,7 @@ namespace abc_bank
 
         private String ToDollars(double d)
         {
-            return String.Format("$%,.2f", Math.Abs(d));
+            return Math.Abs(d).ToString("C");
         }
     }
 }
