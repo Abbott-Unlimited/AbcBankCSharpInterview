83279f34c6025c545ac4d852b03ec715b81e95ae
aaronmontgomery <aaronmontgomery1989@outlook.com>
2023-01-07 17:41:00 -0500

    passing TestApp and TestThreeAccounts CustomerTest tests

--- abc-bank/Customer.cs
+++ abc-bank/Customer.cs
@@ -84,7 +84,7 @@ namespace abc_bank
 
         private String ToDollars(double d)
         {
-            return String.Format("$%,.2f", Math.Abs(d));
+            return String.Format("{0:C2}", Math.Abs(d));
         }
     }
 }
